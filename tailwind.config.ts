28b78df0f375260b6c62d3a848c60a614c68d0ea
Aron Berenyi <aron.berenyi@tuta.io>
2024-05-02 15:44:13 +0200

    initialize the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        brand: {
+          200: '#CCF3E8',
+          450: '#13E5BF',
+          DEFAULT: '#01C38D'
+        } ,
+        petrol: {
+          700: '#364053',
+          800: '#191E29',
+        },
+        purple: {
+          450: '#AD95FF',
+          500: '#7869FF',
+        },
+        blue: {
+          400: '#7ABCFF',
+          500: '#2D95FF'
+        }
+      }
     },
   },
   plugins: [],
